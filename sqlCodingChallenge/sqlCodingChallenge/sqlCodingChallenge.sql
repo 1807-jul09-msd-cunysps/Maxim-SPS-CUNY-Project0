8ef52368d3afb64cc79dd940740ba291b3240dad
maxtillmann <31893405+maxtillmann@users.noreply.github.com>
2018-07-23 14:59:41 -0400

    SQL coding challenge script

---
+++ sqlCodingChallenge/sqlCodingChallenge/sqlCodingChallenge.sql
@@ -0,0 +1,65 @@
+
+CREATE TABLE Products
+    ([ID] VARCHAR(30) PRIMARY KEY,
+     [Name] VARCHAR(30),
+     [Price] INT
+    )
+;
+CREATE TABLE Customers
+    ([ID] VARCHAR(30) PRIMARY KEY,
+     [FirstName] VARCHAR(50),
+	 [LastName] VARCHAR(50),
+     [CardNumber]VARCHAR(30)
+    )
+;
+
+CREATE TABLE ORDERS
+    ([ID] VARCHAR(30) PRIMARY KEY,
+     [ProductID] varchar(30) foreign key references Products(ID),
+     [CustomerID] varchar(30) foreign key references Customers(ID)
+    )
+;
+
+INSERT INTO ORDERS
+    ([ID],[ProductID],[CustomerID])
+
+VALUES
+    ('1','11','111'),
+	('2','11','222'),
+	('3','22','333')
+
+;
+
+INSERT INTO Products
+    ([ID],[Name],[Price])
+
+VALUES
+    ('11','Iphone','200'),
+	('22','Razr','50'),
+	('33','Galaxy s8','1000')
+
+;
+
+INSERT INTO Customers
+    ([ID],[FirstName],[LastName],[CardNumber])
+
+VALUES
+    ('111','John','Doe','1234567'),
+	('222','Tina','Smith','7654321'),
+	('333','Mary','Grace','10101010')
+
+;
+
+SELECT * FROM ORDERS WHERE CustomerID 
+INTERSECT
+SELECT ID FROM Customers WHERE FirstName = 'Tina' AND 
+LastName = 'Smith';
+
+
+
+SELECT (SELECT COUNT(ID) FROM ORDERS WHERE ProductID = (SELECT 
+* FROM Products WHERE Name = 'Iphone'))
+*
+(SELECT Price
+FROM Products
+WHERE Name = 'Iphone'); 
